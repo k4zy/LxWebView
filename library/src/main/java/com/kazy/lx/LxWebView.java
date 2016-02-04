cba7263ff83bb20b785512bb49ff1849e7db65d8
Kouta Imanaka <keima@p-side.net>
2016-02-04 19:23:05 +0900

    Add option WebSettings#setSavePassword(false) for forward compatibility.

--- library/src/main/java/com/kazy/lx/LxWebView.java
+++ library/src/main/java/com/kazy/lx/LxWebView.java
@@ -104,6 +104,10 @@ public class LxWebView extends WebView {
         setting.setSupportMultipleWindows(supportMultipleWindows);
         setting.setSupportZoom(supportZoom);
         setting.setUseWideViewPort(useWideViewPort);
+
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            setting.setSavePassword(false);
+        }
     }
 
     public void addLoadingInterceptor(LoadingInterceptor loadingInterceptor) {
